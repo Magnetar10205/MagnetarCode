78da606504949ee4066f1ea7e52ce667866100bf
Ege <uysalegekaan@gmail.com>
2025-02-17 14:32:34 +0300

    update 3

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -81,17 +81,17 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    // if (driveTrain.gamepad.getRawButton(8)){
-    //   driveTrain.drive.arcadeDrive(0,0.7);
-    // }else if (driveTrain.gamepad.getRawButton(7)){
-    //   driveTrain.drive.arcadeDrive(0,-0.7);
-    // }else{
+    if (driveTrain.gamepad.getRawButton(8)){
+      driveTrain.drive.arcadeDrive(0,0.7);
+    }else if (driveTrain.gamepad.getRawButton(7)){
+      driveTrain.drive.arcadeDrive(0,-0.7);
+    }else{
 
-    //   driveTrain.DriveArcade();
+      driveTrain.DriveArcade();
 
-    // }
+    }
 
-       driveTrain.DriveArcade();
+      //  driveTrain.DriveArcade();
 
 
 
