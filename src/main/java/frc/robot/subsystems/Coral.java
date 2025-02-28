be9886634af487443d3df595fdee1a6d7ae65e97
Emir ÇİÇEK <emircicek1125@gmail.com>
2025-02-28 11:00:36 +0300

    coral düzenlendi

--- src/main/java/frc/robot/subsystems/Coral.java
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -1,55 +1,3 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
-
-// package frc.robot.subsystems;
-
-// import edu.wpi.first.wpilibj.Joystick;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
-// import edu.wpi.first.wpilibj.motorcontrol.Spark;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// public class Coral extends SubsystemBase {
-//   /** Creates a new Coral. */
-//   // Spark ile kontrol edilir. 
-
-//     private Joystick joystick;
-//     private int MotorPort1;
-//     private int MotorPort2;
-//     private PWMVictorSPX intakeMotor1;
-//     private PWMVictorSPX intakeMotor2;
-
-//   public Coral(Joystick joystick, int MotorPort1, int MotorPort2) {
-//       this.joystick = joystick;
-//       this.MotorPort1 = MotorPort1;
-//       this.MotorPort2 = MotorPort2;
-//       intakeMotor1 = new PWMVictorSPX(MotorPort1);
-//       intakeMotor2 = new PWMVictorSPX(MotorPort2);
-//   }
-
-//     public void intakeIn() {
-//       intakeMotor1.set(0.4); // İçeri al
-//       intakeMotor2.set(0.4); // İçeri al
-//   }
-
-//   public void intakeOut() {
-//       intakeMotor1.set(-0.4); // Dışarı at
-//       intakeMotor2.set(-0.4); // Dışarı at
-//   }
-
-//   public void stopMotor() {
-//       intakeMotor1.set(0); // Motoru durdur
-//       intakeMotor2.set(0); // Motoru durdur
-//   }
-
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//   }
-// }
-
-
-
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -61,8 +9,7 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class Coral extends SubsystemBase {
     private Joystick joystick;
-    private int MotorPort1;
-    private int MotorPort2;
+
     private PWMVictorSPX intakeMotor1;
     private PWMVictorSPX intakeMotor2;
     private DigitalInput photoSwitch;
@@ -70,8 +17,6 @@ public class Coral extends SubsystemBase {
 
     public Coral(Joystick joystick, int MotorPort1, int MotorPort2,DigitalInput photoSwitch) {
         this.joystick = joystick;
-        this.MotorPort1 = MotorPort1;
-        this.MotorPort2 = MotorPort2;
         intakeMotor1 = new PWMVictorSPX(MotorPort1);
         intakeMotor2 = new PWMVictorSPX(MotorPort2);
         this.photoSwitch =photoSwitch;
@@ -86,8 +31,6 @@ public class Coral extends SubsystemBase {
         intakeMotor1.set(-0.4);
         intakeMotor2.set(-0.4);
     }
-
-
 
     public void stopMotor() {
         intakeMotor1.set(0);
@@ -105,16 +48,6 @@ public class Coral extends SubsystemBase {
         }
         stopMotor();
         Timer.delay(0.25);
-        // calisti = true;
-
-        // Timer timer = new Timer();
-        // timer.start();
-        // double maxReverseTime = 5.0; 
-        // while (photoSwitch.get() && timer.get() < maxReverseTime ){
-            // intakeIn();
-            
-        // }
-        // stopMotor();
 
         while (photoSwitch.get()){
             intakeIn();
@@ -132,27 +65,6 @@ public class Coral extends SubsystemBase {
         stopMotor();
         timer.delay(0.25);
         
-        // calisti = false;
-
-        // if (photoSwitch.get()) {
-        //     // Motorları ileri al
-        //     intakeIn();
-        //     Timer.delay(1.0);
-        //     stopMotor();
-
-        //     // Geri alma işlemi (5 saniyeye kadar)
-        //     // Timer timer = new Timer();
-        //     // timer.start();
-        //     // double maxReverseTime = 5.0; 
-
-        //     while (!photoSwitch.get() && timer.get() < maxReverseTime) {
-        //         intakeOut();
-        //     }
-        //     stopMotor();
-
-        //     timer.stop();
-        //     timer.reset();
-        // }
 
         isRunning = false; // İşlem tamamlandığında sıfırla
     }
@@ -165,7 +77,7 @@ public class Coral extends SubsystemBase {
         }
 
         boolean sensor_boolen = !photoSwitch.get();
-// photo = true !photo = false (boştayken)
+        // photo = true !photo = false (boştayken)
         SmartDashboard.putBoolean("Sensör Coral", sensor_boolen );
     }
 }
